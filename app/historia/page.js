cef4c0631eb4f92f12d71ee04b6f9682cb11a2f5
LJprogram657 <ljcodesystems@gmail.com>
2026-02-26 15:22:45 -0500

    actulizacion de prueba

--- app/historia/page.js
+++ app/historia/page.js
@@ -5,7 +5,7 @@ export default function Historia() {
         <h2 className="section__title">Historia</h2>
         <p>El Colegio Nuestra Señora de la Sabiduría es una institución educativa privada y católica, fundada en la ciudad de Acacías el 3 de diciembre de 1947.</p>
         <p>Su funcionamiento está aprobado por la Secretaría de Educación del Meta, inspirada por San Luis María Grignion de Montfort y la Beata María Luisa de Jesús Trichet.</p>
-        <p>Forma personas comprometidas con una sociedad más justa.</p>
+        <p>Forma personas comprometidas con una sociedad.</p>
       </div>
     </main>
   )
